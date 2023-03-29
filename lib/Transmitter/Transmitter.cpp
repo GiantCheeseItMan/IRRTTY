56c3474263d3d07ce4e2c06e292e485233eaec29
Corey C <giantcheeseitman@gmail.com>
2023-03-29 17:42:15 -0400

    Moving functions from ino into transmitter

--- lib/Transmitter/Transmitter.cpp
+++ lib/Transmitter/Transmitter.cpp
@@ -1,1 +1,44 @@
 #include "Transmitter.h"
+
+/**
+ * Converts an input string into UART bits
+*/
+String Ascii2UART(String input)
+{
+  String output = "";
+  for (unsigned int i = 0; i < input.length(); i++)
+  {
+    output += START_BIT;
+
+    for (int ii = 0; ii < 8; ii++)
+    {
+      byte bytes = bitRead(input[i], ii); //Turns the character into a byte
+      output += bytes;
+    }
+    output += STOP_BIT;
+  }
+  return output;
+}
+
+bool transmit(String string)
+{
+    bool done = false;
+     String rec = Ascii2UART(string);
+    //turn bits into square wave
+    for (unsigned int i = 0; i < rec.length(); i++)
+    {
+      delay(BIT_DELAY_TIME);
+
+      if (rec[i] == '0')
+      {
+        tone(9, SPACE_FREQUENCY);
+      }
+      else
+      {
+
+        tone(9, MARK_FREQUENCY);
+      }
+    }
+    return done;
+}
+
--- lib/Transmitter/Transmitter.h
+++ lib/Transmitter/Transmitter.h
@@ -3,4 +3,15 @@
 
 #include "Arduino.h"
 
+#define START_BIT '0'
+#define STOP_BIT '1'
+#define MARK_FREQUENCY 2125
+#define SPACE_FREQUENCY 2295
+#define BAUD_RATE 45.45
+#define BIT_DELAY_TIME 1000 / BAUD_RATE
+
+String Ascii2UART(String input);
+
+
+
 #endif // TRANSMITTER_H
