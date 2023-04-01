7063c5d047494450d74ce0572bfe4ad4e118b44c
Corey C <giantcheeseitman@gmail.com>
2023-04-01 15:26:27 -0400

    Inlcuded settings.h in transmitter

--- lib/Transmitter/Transmitter.h
+++ lib/Transmitter/Transmitter.h
@@ -2,13 +2,7 @@
 #define TRANSMITTER_H
 
 #include "Arduino.h"
-
-#define START_BIT '0'
-#define STOP_BIT '1'
-#define MARK_FREQUENCY 2125
-#define SPACE_FREQUENCY 2295
-#define BAUD_RATE 45.45
-#define BIT_DELAY_TIME 1000 / BAUD_RATE
+#include "Settings.h"
 
 String Ascii2UART(String input);
 
