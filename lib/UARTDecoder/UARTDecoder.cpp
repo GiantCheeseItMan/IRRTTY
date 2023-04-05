924f911cb97bc0dba0ff05d0a21937750200069c
Corey C <giantcheeseitman@gmail.com>
2023-04-05 16:51:07 -0400

    Chose decoder implementation

--- lib/UARTDecoder/UARTDecoder.cpp
+++ lib/UARTDecoder/UARTDecoder.cpp
@@ -2,58 +2,6 @@
 #include "FrequencyDetector.h"
 #include <avr/interrupt.h>
 
-//put recieved bits into an array
-
-char freqinput=='0'; //change, should involve interrupt
-
-bool arr[10]= {false}
-for (int i=0;i<10;i++)
-{
-    arr[i] = (freqinput == '1');
-}
-
-if (i==9)
-{
-    bool databits[8] = {false}
-    for (int ii=0; ii<8; ii++)
-    {
-        databits[ii] = arr[ii+1];
-    }
-}
-
-    
-//timer
-//when first 0 is detected, record time
-//unsigned long first0 = millis();
-//0CIE0A bit = 1
-//cli(); // disable interrupts
-//TCCR1A = 0; //timer control registers
-//TCCR1B = 0;
-//TCNT1 = 0; //timer count register
-//OCR1A = 625; //sets 45.45 BAUD
-//TCCR1B |= (1 << WGM12); // set timer to CTC (Clear Timer on Compare Match) mode
-//TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start timer
-//TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-//sei(); // enable interrupts
-
-char freqinput=='0'; //change, should involve interrupt
-
-bool arr[10]= {false}
-for (int i=0;i<10;i++)
-{
-    arr[i] = (freqinput == '1');
-}
-
-if (i==9)
-{
-    bool databits[8] = {false}
-    for (int ii=0; ii<8; ii++)
-    {
-        databits[ii] = arr[ii+1];
-    }
-}
-
-
 Decoder::Decoder()
 {
     decoderCursor = 0;
